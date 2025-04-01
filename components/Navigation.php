df439ad871f15654bb5837711929dcb6ad08ce6f
victorgalvez0312@gmail.com <eugamer00159@gmail.com>
2025-04-01 11:28:53 -0400

    Estructura base: HTML, CSS y JS

---
+++ components/Navigation.php
@@ -0,0 +1,26 @@
+    <!-- Masthead-->
+    <header class="masthead">
+            <div class="container">
+            <div class="masthead-subheading">¡Bienvenido a tu tienda de confianza de los mejores libros del mercado!</div>
+            <div class="masthead-heading text-uppercase">Variedad, calidad y los mejores precios</div>
+            </div>
+    </header>
+        
+<!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top"><img src="assets\img\logocasalibros.png" alt="Logo de la empresa." /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#page-top">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Nuestros Libros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#ofertas">Nuestros Autores</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Comentarios</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
